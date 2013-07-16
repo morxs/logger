f1b11a6d4c6f9403ae5c0d7e19c8633564640784
morxs <free4spams@yahoo.com>
2013-07-16 20:03:08 +0700

    normal log query didnt use allow origin

--- logger.js
+++ logger.js
@@ -6,10 +6,10 @@ var EXCFILENAME = 'exceptions.log';
 
 var logger = new (winston.Logger)({
     transports: [
-		new (winston.transports.Console)({ colorize: true }),
-		new (winston.transports.File)({ filename: LOGFILENAME,
-		                                maxsize: 10000000,
-		                                maxFiles: 5 })
+    new (winston.transports.Console)({ colorize: true }),
+    new (winston.transports.File)({ filename: LOGFILENAME,
+                                    maxsize: 10000000,
+                                    maxFiles: 5 })
     ],
     exceptionHandlers: [
       new winston.transports.File({ filename: EXCFILENAME })
@@ -32,35 +32,35 @@ app.use(express.bodyParser());
 
 /*
 app.get('/hello.txt', function(req, res){
-	var body = 'Hello World';
-	
-	res.setHeader('Content-Type', 'text/plain');
-	res.setHeader('Content-Length', body.length);
-	res.end(body);
+  var body = 'Hello World';
+  
+  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Length', body.length);
+  res.end(body);
 });
 */
 
 app.get('/query_log', function(req, res){
-	winston.query(options, function (err, results) {
-  		if (err) {
-   			err;
-  		}
-  		
-  		//console.log(results);
-  		var body = JSON.stringify(results);
-  		res.setHeader('Content-Type', 'text/json');
-  		res.setHeader('Content-Length', body.length);
-      res.setHeader('Access-Control-Allow-Origin', '*');
-		  res.end(body);
-  	});
+  winston.query(options, function (err, results) {
+      if (err) {
+        err;
+      }
+      
+      //console.log(results);
+      var body = JSON.stringify(results);
+      res.setHeader('Content-Type', 'text/json');
+      res.setHeader('Content-Length', body.length);
+      //res.setHeader('Access-Control-Allow-Origin', '*');
+      res.end(body);
+    });
 })
 
 app.post('/logmein', function(req, res){
-	//logger.log('info', 'logmein - Hello distributed log files');
-	var log_msg = req.body.logmsg;
-	logger.log('error', '%s - %s', req.ip, log_msg);
+  //logger.log('info', 'logmein - Hello distributed log files');
+  var log_msg = req.body.logmsg;
+  logger.log('error', '%s - %s', req.ip, log_msg);
   res.setHeader('Access-Control-Allow-Origin', '*');
-	res.end();
+  res.end();
 });
 
 app.listen(3000);
